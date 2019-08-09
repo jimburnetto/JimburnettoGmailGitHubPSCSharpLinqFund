e4a612bcea239e8a591b5e697e5c7c3258cac913
Jim Burnett <jburnett@tenaska.com>
2019-08-09 16:13:37 -0500

    added materials to source control

---
+++ 2-linq-fundamentals-csharp-6-m2-exercise-files/before/Features/MyLinq.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.Linq
+{
+    public static class MyLinq
+    {
+        /// <summary>
+        /// Test extension of Count to demonstrate how to use an extension similar to linq
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sequence"></param>
+        /// <returns> count </returns>
+        public static int Count<T>(this IEnumerable<T> sequence)
+        {
+            int count = 0;
+
+            foreach (var item in sequence)
+            {
+                count += 1;
+            }
+            return count;
+        }
+    }
+}
---
+++ 2-linq-fundamentals-csharp-6-m2-exercise-files/before/Features/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using Features.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> add = (x, y) =>
+            {
+                int temp = x + y;
+                return temp;
+            };
+
+            Action<int> write = x => Console.WriteLine(x);
+
+            write(square(add(3,5)));
+
+            //Employee[] developers = new Employee[]
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee { Id = 1, Name= "Scott" },
+                new Employee { Id = 2, Name= "Chris" }
+            };
+
+            //List<Employee> sales = new List<Employee>()
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex" }
+            };
+
+            foreach (var employee in developers.Where(
+                            e => e.Name.StartsWith("S")
+                ))
+            {
+                Console.WriteLine(employee.Name);
+            }
+            Console.ReadLine();
+        }
+
+  
+        private static bool NameStartsWithS(Employee employee)
+        {
+            return employee.Name.StartsWith("S");
+        }
+    }
+}
