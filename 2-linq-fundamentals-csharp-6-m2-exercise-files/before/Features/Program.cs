d8e28e22c14926651882e8415213cc3094421a4e
Jim Burnett <jburnett@tenaska.com>
2019-08-13 08:26:36 -0500

    method vs query syntax

--- 2-linq-fundamentals-csharp-6-m2-exercise-files/before/Features/Program.cs
+++ 2-linq-fundamentals-csharp-6-m2-exercise-files/before/Features/Program.cs
@@ -23,22 +23,28 @@ namespace Features
             write(square(add(3,5)));
 
             //Employee[] developers = new Employee[]
-            IEnumerable<Employee> developers = new Employee[]
+            var developers = new Employee[]
             {
                 new Employee { Id = 1, Name= "Scott" },
                 new Employee { Id = 2, Name= "Chris" }
             };
 
             //List<Employee> sales = new List<Employee>()
-            IEnumerable<Employee> sales = new List<Employee>()
+            var sales = new List<Employee>()
             {
                 new Employee { Id = 3, Name = "Alex" }
             };
 
-            foreach (var employee in developers.Where( e => e.Name.Length == 5 )
-                                               .OrderBy(e => e.Name )
-                
-                )
+            var query = developers.Where(e => e.Name.Length == 5)
+                                               .OrderBy(e => e.Name);
+
+            var query2 = from developer in developers
+                         where developer.Name.Length == 5
+                         orderby developer.Name
+                         select developer;
+
+
+            foreach (var employee in query2)
 
             {
                 Console.WriteLine(employee.Name);
